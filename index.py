d55f090cd1ea0c4ee11cf71be327a62271f713cd
Gustavo Henrique de Melo Bezerra Brandão <20181174010024@ifrn.local>
2018-11-21 16:07:35 -0300

    Add the first templates

--- index.py
+++ index.py
@@ -1,10 +1,17 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello world"
+    return render_template('index.html')
 
 @app.route("/about")
 def hello():
     return "Projeto para expor dados públicos minerados"
+
+@app.route("/home")
+
+@app.route("/home/<user>")
+def home(user):
+    return render_template('home.html',user=user)
